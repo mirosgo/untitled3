dc0ec0bcfbfb516737de5cf7fcd54ddc4c96ef24
mirgo <rimlog.94@gmail.com>
2021-12-13 21:21:41 +0300

    Initial commit

---
+++ src/main/java/Directory.java
@@ -0,0 +1,20 @@
+public class Directory {
+    private Map<String, List<String>> directory_hm = new HashMap<>();
+    private List<String> phone_number_list;
+
+    public void add(String surname, String phone_number) {
+        if (directory_hm.containsKey(surname)) {
+            phone_number_list = directory_hm.get(surname);
+            phone_number_list.add(phone_number);
+            directory_hm.put(surname, phone_number_list);
+        } else {
+            phone_number_list = new ArrayList<>();
+            phone_number_list.add(phone_number);
+            directory_hm.put(surname, phone_number_list);
+        }
+    }
+
+    public List<String> get(String surname) {
+        return directory_hm.get(surname);
+    }
+}
